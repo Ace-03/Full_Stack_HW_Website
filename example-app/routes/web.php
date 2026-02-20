064d040b2194e076e9d263c435761b21879cf3cb
6185jcaron <66760646+Ace-03@users.noreply.github.com>
2026-02-19 17:56:26 -0700

    updated example app

---
+++ example-app/routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Mail\JobPosted;
+use App\Models\Employer;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use GuzzleHttp\Promise\Create;
+use App\Models\Job;
+
+Route::get('test', function () {
+
+    $job=Job::first();
+
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
+
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+
+Route::get('/employers', function () {
+    $employers = Employer::with('jobs')->paginate(20);
+
+    return view('/employers',[
+        'employers' => $employers
+    ]);
+});
+
+route::get('/employers/{id}', function ($id) {
+   $employer = Employer::find($id);
+
+   return view('employer', ['employer' => $employer]);
+});
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
